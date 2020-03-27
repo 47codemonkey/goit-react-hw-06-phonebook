9709075c0e999cf4effcb12d1b2f25c3ab90a403
Алексей Мороз <alexeymoroz@Alex-Moroz.local>
2020-03-27 15:37:06 +0200

    changes

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { phoneBookReducer } from './phoneBookReducer';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
-const store = configureStore({
-  reducer: phoneBookReducer,
-});
+const store = configureStore(
+  {
+    reducer: phoneBookReducer,
+  },
+  devToolsEnhancer(),
+);
 
 export default store;
